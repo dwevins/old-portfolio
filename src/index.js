1bb63bc6e16eabd852cf5ad6360189af88abd72f
Danny Evins <woody.evins@gmail.com>
2016-09-29 09:13:03 -0500

    migrating files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-'use strict';
+'use strict'
+import ProjectList from 'projectList'
+import ProjectsView from 'projectsView'
+
+export default function Index(app) {
+  const projectList = ProjectList();
+  ProjectsView(projectList);
+}
---
+++ src/project.js
@@ -0,0 +1,12 @@
+'use strict'
+
+export default class Project {
+  constructor(project) {
+    const name = project.name;
+    const url = project.url;
+    const github = project.github;
+    const thumb = project.thumb;
+    const image = project.image;
+    const description = project.description;
+  }
+}
