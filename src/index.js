6f0a111d5b63f8a5c07f336390cc5287816f08ed
Danny Evins <woody.evins@gmail.com>
2016-10-02 12:35:23 -0500

    require works

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
-import ProjectList from 'projectList';
+import projectsList from 'projectList';
 import ProjectsView from 'projectsView';
 
 export default function Index(app) {
-  const projectList = ProjectList();
-  const view = ProjectsView(projectList, app);
+  const projectList = projectsList();
+  const view = new ProjectsView(projectList, app);
   view.render();
 }
