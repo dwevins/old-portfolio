5b37e01360fe263ca30ae194f46b33a9114cbd8c
Danny Evins <woody.evins@gmail.com>
2016-10-02 16:30:33 -0500

    move project rendering to project class

--- src/project.js
+++ src/project.js
@@ -1,12 +1,40 @@
 'use strict';
 
 export default class Project {
-  constructor(project) {
+  constructor(project, parent) {
+    this.parent = parent;
     this.name = project.name;
     this.url = project.url;
     this.github = project.github;
     this.thumb = project.thumb;
     this.image = project.image;
     this.description = project.description;
+  }
+
+  render() {
+
+  }
+
+  generateBoxSkeleton() {
+    const projectBox = document.createElement('div');
+    projectBox.classList = 'column is-one-third';
+    projectBox.innerHTML = `
+      <img class="preload">
+      <div class="project-list__item box">
+        <div class="project-list__item--img-container">
+          <img class="project-img">
+        </div>
+        <div class="project-list__item--button button is-info is-one-third">
+          more info
+        </div>
+      </div>`;
+  }
+
+  populateBox() {
+
+  }
+
+  openModal() {
+
   }
 }
--- src/projectsView.js
+++ src/projectsView.js
@@ -3,7 +3,7 @@ import Project from 'project';
 
 export default class ProjectsView {
   constructor(projectList, app) {
-    this.projectList = projectList.map((project) => new Project(project));
+    this.projectList = projectList;
     this.app = app;
   }
 
@@ -12,28 +12,13 @@ export default class ProjectsView {
     let columns = document.querySelector('.projects-columns');
 
     for (let i = 0; i < this.projectList.length; i++) {
-      const projectBox = document.createElement('div');
-      projectBox.classList = 'column is-one-third';
-      projectBox.innerHTML = `
-        <img class="preload">
-        <div class="project-list__item box">
-          <div class="project-list__item--img-container">
-            <img class="project-img">
-          </div>
-          <div class="project-list__item--button button is-info is-one-third">
-            more info
-          </div>
-        </div>`;
-
-      projectBox.querySelector('.preload').setAttribute('src', this.projectList[i].image);
-      projectBox.querySelector('.project-img').setAttribute('src', this.projectList[i].thumb);
-
       if (i > 0 && i % 3 === 0) {
         columns = document.createElement('div');
         columns.classList = 'columns projects-columns';
         container.appendChild(columns);
       }
-      columns.appendChild(projectBox);
+      const project = new Project(this.projectList[i], columns);
+      project.render;
     }
   }
 }
