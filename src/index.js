120233c6d4bc62ada4214440339ae01e8be83ba5
Danny Evins <woody.evins@gmail.com>
2016-10-02 17:12:15 -0500

    contact controls work, going live

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ export default function Index(app) {
   const projectList = projectsList();
   const view = new ProjectsView(projectList, app);
   view.render();
+
+  document.querySelector('.github-contact').addEventListener('click', () => {
+    window.open('http://www.github.com/dwevins', '_blank');
+  });
+
+  document.querySelector('.email-contact').addEventListener('click', () => {
+    document.location.href = 'mailto:woody.evins@gmail.com';
+  });
+
+  document.querySelector('.linkedin-contact').addEventListener('click', () => {
+    window.open('http://www.linkedin.com/in/woodyevins', '_blank');
+  });
 }
